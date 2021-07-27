48db2c76e1ce2fd38ad20d6bb1963829ddf097ed
kota.abe <akreemployment@gmail.com>
2021-07-27 18:57:38 +0900

    getAllMessagesを追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "tasks")
 public class Task {
     @Id
@@ -17,7 +25,7 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    /*
+/*
     @Column(name = "title", length = 255, nullable = false)
     private String title;
 */
@@ -39,7 +47,7 @@ public class Task {
         this.id = id;
     }
 
-    /*
+/*
     public String getTitle() {
         return title;
     }
